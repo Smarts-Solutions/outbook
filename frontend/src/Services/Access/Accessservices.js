9e1cf6cde753d8143031d26d558b173d2fe63434
snehpnp <snehpnp@gmail.com>
2024-07-08 17:37:27 +0530

    Done Api Integrate

---
+++ frontend/src/Services/Access/Accessservices.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+// import Files
+import * as Config from "../../Utils/Config";
+import { header } from "../../Utils/ApiHeader";
+
+
+// Role Action API Add, Edit, Delete
+export async function GETACCESS(data, token) {
+    try {
+        const res = await axios.post(`${Config.base_url}accessRolePermissions`, data, {
+            headers: header(token),
+            data: {}
+        })
+        return await res?.data;
+    }
+    catch (err) {
+        return await err;
+    }
+}
