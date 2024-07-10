fc89329a31a1894b9e6127ac6d6b90bdd359aae9
snehpnp <snehpnp@gmail.com>
2024-07-10 10:25:37 +0530

    Done

--- frontend/src/Components/Dashboard/Sidebar.jsx
+++ frontend/src/Components/Dashboard/Sidebar.jsx
@@ -107,7 +107,7 @@ const Sidebar = () => {
                 <div className="simplebar-content-wrapper" style={{ height: "100%", overflow: "hidden scroll" }}>
                   <div className="simplebar-content" style={{ padding: "0px 0px 70px" }}>
                     <ul className="metismenu left-sidenav-menu mm-show">
-
+                      {/* 
                       <li className={activeLink === '/dashboard' ? 'active' : ''}>
                         <Link
                           to="/admin/dashboard"
@@ -205,7 +205,108 @@ const Sidebar = () => {
                           </span>
                           <span>Setting</span>
                         </Link>
-                      </li>}
+                      </li>} */}
+
+
+
+                      <li className={activeLink === '/dashboard' ? 'active' : ''}>
+                        <Link
+                          to="/admin/dashboard"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/dashboard' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/dashboard')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/dashboard.png" alt="Dashboard" />
+                          </span>
+                          <span>Dashboard</span>
+                        </Link>
+                      </li>
+
+                   <li className={activeLink === '/admin/customer' ? 'active' : ''}>
+                        <Link
+                          to="/admin/customer"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/customer' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/customer')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/customers.png" alt="Customer" />
+                          </span>
+                          <span>Customer</span>
+                        </Link>
+                      </li>
+
+                      <li className={activeLink === '/admin/status' ? 'active' : ''}>
+                        <Link
+                          to="/admin/status"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/status' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/status')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/status.png" alt="Status" />
+                          </span>
+                          <span>Status</span>
+
+                        </Link>
+                      </li>
+
+                     <li className={activeLink === '/admin/staff' ? 'active' : ''}>
+                        <Link
+                          to="/admin/staff"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/staff' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/staff')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/staff.png" alt="Staff" />
+                          </span>
+                          <span>Staff</span>
+                        </Link>
+                      </li>
+
+                      <li className={activeLink === '/admin/access' ? 'active' : ''}>
+                        <Link
+                          to="/admin/access"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/access' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/access')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/access.png" alt="Access" />
+                          </span>
+                          <span>Access</span>
+                        </Link>
+                      </li>
+
+                      <li className={activeLink === '/admin/reports' ? 'active' : ''}>
+                        <Link
+                          to="/admin/reports"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/reports' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/reports')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/reports.png" alt="Report" />
+                          </span>
+                          <span>Report</span>
+                        </Link>
+                      </li>
+
+                      <li className={activeLink === '/admin/setting' ? 'active' : ''}>
+                        <Link
+                          to="/admin/setting"
+                          aria-expanded="false"
+                          className={activeLink === '/admin/setting' ? 'active' : ''}
+                          onClick={(e) => handleLinkClick(e, '/admin/setting')}
+                        >
+                          <span className="sidebar-icons">
+                            <img src="/assets/images/sidebar-icons/setting.png" alt="Setting" />
+                          </span>
+                          <span>Setting</span>
+                        </Link>
+                      </li>
 
 
                     </ul>
