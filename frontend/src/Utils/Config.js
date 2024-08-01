6c07ceafb65255549f78a5668e28639d2d9ccfba
SHKGIT007 <shkgit007@gmail.com>
2024-08-01 16:45:01 +0530

    done

--- frontend/src/Utils/Config.js
+++ frontend/src/Utils/Config.js
@@ -2,6 +2,7 @@
 // export const base_url = `${window.location.origin}/backend/`;
 export const base_url = `http://localhost:2222/`;
 // export const base_url = `http://185.209.75.6:2222/`;
+//  export const base_url = `${window.location.origin}/backend/`
 
  
 
