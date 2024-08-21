a00c18dadcf4e833626cdd5e097e0b98e74c7204
NehaSharma3496 <sharmaneha3496@gmail.com>
2024-08-21 17:50:52 +0530

    Done

--- frontend/src/layouts/Admin/customer/customer_edit_process/Engagement.jsx
+++ frontend/src/layouts/Admin/customer/customer_edit_process/Engagement.jsx
@@ -453,7 +453,6 @@ const Engagement = () => {
 
                     <div className={"details__wrapper"}>
 
-
                         <div className="card report-data pricing-box  p-0">
                             <div className="card-header step-header-blue">
                             <h4
