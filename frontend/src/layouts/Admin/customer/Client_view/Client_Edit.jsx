568df4a31cfbd3ae876a87bb2654acf268c45402
NehaSharma3496 <sharmaneha3496@gmail.com>
2024-08-29 18:41:34 +0530

    done

--- frontend/src/layouts/Admin/customer/Client_view/Client_Edit.jsx
+++ frontend/src/layouts/Admin/customer/Client_view/Client_Edit.jsx
@@ -904,15 +904,16 @@ const ClientEdit = () => {
         <div className="row">
           <div className="col-xl-12">
             <div className="card">
-              <div className="card-header align-items-center step-header-blue d-flex justify-content-between">
-                <h4 className="card-title  mb-0">Edit Client</h4>
+              <div className="card-header align-items-center step-header-blue d-flex ">
+                
                 <button
                   type="button"
-                  className="btn btn-info text-white blue-btn"
+                  className="btn p-0"
                   onClick={HandleCancel}
                 >
-                  Back
+                 <i className="pe-2 fa-regular fa-arrow-left-long text-white" ></i>
                 </button>
+                <h4 className="card-title  mb-0">Edit Client</h4>
               </div>
 
               {/* end card header */}
