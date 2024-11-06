687de42bf3af9ba33a7d056fc9ca1f64e5598f2d
SHKGIT007 <shkgit007@gmail.com>
2024-11-06 19:00:42 +0530

    Done

--- backend/app/models/reportModel.js
+++ backend/app/models/reportModel.js
@@ -349,7 +349,7 @@ const teamMonthlyReports = async (Report) => {
 
 const dueByReport = async (Report) => {
     const {StaffUserId } = Report;
-    
+
     try {
         const QueryRole = `
   SELECT
@@ -455,6 +455,7 @@ const dueByReport = async (Report) => {
 `;
 
         const [result] = await pool.execute(query);
+        console.log("result ", result);
         return { status: true, message: 'Success.', data: result };
     }else{
         return { status: true, message: 'Success.', data: [] };
