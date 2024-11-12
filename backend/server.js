baeb15d8dc0946a64eb6f4100c1806a2ce50e06a
SHKGIT007 <shkgit007@gmail.com>
2024-11-12 18:22:07 +0530

    Done

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const app = require('./app');
 const http = require('http');
 const { port } = require('./app/config/config');
 
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
