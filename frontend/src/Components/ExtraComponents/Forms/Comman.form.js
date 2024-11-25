fe8c601a7acde8daf3ccc4224dfb1e98cf8560f4
Chandra6388 <cppatel6388@gmail.com>
2024-11-25 19:00:56 +0530

    Refactor form field types to include select2 and number1 for improved functionality

--- frontend/src/Components/ExtraComponents/Forms/Comman.form.js
+++ frontend/src/Components/ExtraComponents/Forms/Comman.form.js
@@ -144,7 +144,42 @@ const ReusableForm = ({
                     </div>
                   </div>
                 </>
-              ) : field.type === "checkbox" ? (
+              ) : field.type === "select2" ? (
+                <>
+                  <div className={`col-lg-${title === "update_theme" ? 12 : field.col_size}`}  >
+                    <div className=" row">
+                      <label className={`col-lg-${field.label_size}`} htmlFor={field.name} >
+                        {field.label}
+                       
+                      </label>
+                      <div className={`col-lg-${title === "addgroup" ? 12 : 12}`}  >
+                        <select
+                          className={formik.touched[field.name] && formik.errors[field.name] ? "default-select wide error-field form-select" : " default-select wide form-select"}
+                          id={field.name}
+                          autoFocus={index === 0 ? true : false}
+                          style={{ background: field.disable ? "#eeeeee" : "" }}
+                          {...formik.getFieldProps(field.name)}
+                          disabled={field.disable}
+                        >
+                          
+                          {field.options.map((option) => (
+                            <option key={option.value} value={option.value}>
+                              {option.label}
+                            </option>
+                          ))}
+                        </select>
+                        {formik.touched[field.name] &&
+                          formik.errors[field.name] && (
+                            <div className="error-text">
+                              {formik.errors[field.name]}
+                            </div>
+                          )}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ) 
+              : field.type === "checkbox" ? (
                 <>
                   {field.options && field.options.length > 0 ? (
                     <>
@@ -541,7 +576,44 @@ const ReusableForm = ({
                     </div>
                   </div>
                 </>
-              ) : field.type === "text1" ? (
+              )  : field.type === "number1" ? (
+                <>
+                  <div className={`col-lg-${field.col_size}`}>
+                    <div className="mb-3 row flex-column">
+                      <label
+                        className={`col-lg-${field.label_size}`}
+                        htmlFor={field.name}
+                      >
+                        {field.label}
+                        
+                      </label>
+                      <div>
+                        <input
+                          type="number"
+                          className={formik.touched[field.name] && formik.errors[field.name] ? "form-control error-field" : " form-control"}
+                          style={{ background: field.disable ? "#eeeeee" : "" }}
+                          id={field.name}
+                          placeholder={`Enter ${field.label}`}
+                          {...formik.getFieldProps(field.name)}
+                          defaultValue=""
+                          readOnly={field.disable}
+                          autoComplete="new-email"
+                        />
+                        <div className="invalid-feedback">
+                          Please enter {field.label}
+                        </div>
+                        {formik.touched[field.name] &&
+                          formik.errors[field.name] && (
+                            <div className="error-text">
+                              {formik.errors[field.name]}
+                            </div>
+                          )}
+                      </div>
+                    </div>
+                  </div>
+                </>
+              )
+               : field.type === "text1" ? (
                 <>
                   <div className={`col-lg-${field.col_size}`}>
                     <div className="mb-3 row flex-column">
