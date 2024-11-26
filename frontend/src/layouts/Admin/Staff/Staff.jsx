8ba4e801f487db3a7e182d7464435d979770b1aa
Chandra6388 <cppatel6388@gmail.com>
2024-11-26 14:45:29 +0530

    Enable role selection in Staff component by removing disable condition

--- frontend/src/layouts/Admin/Staff/Staff.jsx
+++ frontend/src/layouts/Admin/Staff/Staff.jsx
@@ -432,7 +432,8 @@ const StaffPage = () => {
       label: "Role",
       label_size: 12,
       col_size: 6,
-      disable: editShowModel == true ? true : false,
+      disable: false,
+    //  disable: editShowModel == true ? true : false,
       options:
         roleDataAll &&
         roleDataAll.data.map((data) => {
