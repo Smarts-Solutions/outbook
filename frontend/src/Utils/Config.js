cee84096a494620a31b7a22b5aed3a8ea2277ec4
SHKGIT007 <shkgit007@gmail.com>
2025-02-07 12:21:01 +0530

    Done

--- frontend/src/Utils/Config.js
+++ frontend/src/Utils/Config.js
@@ -1,5 +1,5 @@
-// export const base_url = `${window.location.origin}/backend/`;
- export const base_url = `http://localhost:2222/`;
+export const base_url = `${window.location.origin}/backend/`;
+//  export const base_url = `http://localhost:2222/`;
 // export const base_url = `http://185.209.75.6:2222/`;
 // export const base_url = `https://outbookstest.tradestreet.in/backend/`;
 // export const base_url = `https://outbooks.tradestreet.in/backend/`;
