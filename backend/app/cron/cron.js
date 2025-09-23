d637f3ca76e3e8b03ae8cc38e899163825a93092
SHKGIT007 <shkgit007@gmail.com>
2025-09-23 12:19:49 +0530

    Done

--- backend/app/cron/cron.js
+++ backend/app/cron/cron.js
@@ -11,6 +11,7 @@ module.exports = (app) => {
 
 // Missing Timesheet Report Email to Individual Staff on every Monday at 09:00 AM
   cron.schedule("0 9 * * 1", async () => {
+    console.log("Running a task every Monday at 09:00 AM to send individual emails");
   //cron.schedule("20 11 * * *", async () => {
    const [staffResult] = await pool.query(`
     SELECT 
@@ -22,20 +23,39 @@ module.exports = (app) => {
     WHERE status = '1'
     `);
   // console.log("staffResult , ",staffResult); 
-    sendEmailInWorker(staffResult || []);
-    console.log("Running a task every Monday at 09:00 AM to send individual emails");
+    sendEmailInWorkerMissingTimeSheet(staffResult || []);
+
+
+    ////////-----------Submit Timesheet Reminder Email --------------------//////
+
+   const [staffResultSubmitTimeSheet] = await pool.query(`
+    SELECT 
+        staffs.id AS id,
+        CONCAT(first_name,' ',last_name) AS staff_fullname,
+        staffs.email AS staff_email
+    FROM staffs
+    JOIN roles ON roles.id = staffs.role_id
+    LEFT JOIN timesheet 
+        ON staffs.id = timesheet.staff_id 
+      AND (timesheet.submit_status = '1' OR (roles.id = 1 OR roles.id = 2 OR roles.id = 8)) 
+    WHERE ((roles.id = 1 OR roles.id = 2 OR roles.id = 8) OR timesheet.submit_status = '1') AND staffs.status = '1'
+    GROUP BY staffs.id
+    `);
+
+  sendEmailInWorkerSubmitTimesheet(staffResultSubmitTimeSheet || []);
+
+  
 
 
 
-    
   })
 
 };
 
 
 
-
-function sendEmailInWorker(rows) {
+ // function to send email in worker thread for Missing Timesheet Report
+function sendEmailInWorkerMissingTimeSheet(rows) {
   const worker = new Worker(join(__dirname, "missingTimesheetReportEmail.js"), { type: "module" });
   worker.postMessage(rows); 
   worker.on("message", (msg) => {
@@ -47,3 +67,17 @@ function sendEmailInWorker(rows) {
   });
 }
 
+
+
+// function to send email in worker thread for Submit Timesheet Reminder Email
+function sendEmailInWorkerSubmitTimesheet(rows) {
+  const worker = new Worker(join(__dirname, "submitTimesheetReminderEmail.js"), { type: "module" });
+  worker.postMessage(rows);
+  worker.on("message", (msg) => {
+    console.log("RECEIVED MSG EMAIL SENT--",msg)
+  });
+  worker.on("error", (err) => console.log("Worker error --:", err));
+  worker.on("exit", (code) => {
+    if (code !== 0) console.log(`Worker stopped with exit code ${code}`);
+  });
+}
--- backend/app/cron/submitTimesheetReminderEmail.js
+++ backend/app/cron/submitTimesheetReminderEmail.js
@@ -1,16 +1,15 @@
-// missingTimesheetReportEmail.js
+// submitTimesheetReminderEmail.js
 const { parentPort } = require("worker_threads");
 const pool = require('../config/database');
-const { missingTimesheetReport } = require("../models/reportModel");
 const { commonEmail } = require("../utils/commonEmail");
 
 
 
-// Missing Timesheet Report Email Worker
+// Submit Timesheet Report Email Worker
 parentPort.on("message", async (rows) => {
   for (const row of rows) {
     try {
-            const [[getStaffNameMissingReport]] = await pool.query(`CALL GetLastWeekMissingTimesheetReport(${row.id})`);
+            const [[getStaffNameMissingReport]] = await pool.query(`CALL GetLastWeekSubmitTimesheetReport(${row.id})`);
             //console.log("getStaffNameMissingReport , ",getStaffNameMissingReport); 
             
             let csvContent = "Staff Name,Staff Email\n";
@@ -20,19 +19,19 @@ parentPort.on("message", async (rows) => {
 
            
             let toEmail = row.staff_email;
-            let subjectEmail = "Missing Timesheet Report"
-            let htmlEmail = "<h3>Please find the attached Missing Timesheet Report.</h3>"
+            let subjectEmail = "Submit Timesheet Reminder Report"
+            let htmlEmail = "<h3>Please find the attached Submit Timesheet Report.</h3>"
             const dynamic_attachment = csvContent;
-            const filename = "MissingTimesheetReport.csv";
+            const filename = "SubmitTimesheetReport.csv";
 
            // parentPort.postMessage(`CSV Content for ${row.id}:\n ${csvContent}`);
 
             const emailSent = await commonEmail(toEmail, subjectEmail, htmlEmail, "", "", dynamic_attachment , filename);
             if (emailSent) {
-                //console.log("Missing Timesheet Report email sent successfully.");
+                //console.log("Submit Timesheet Report email sent successfully.");
                 parentPort.postMessage(`✅ Email sent to: ${row.staff_email}`);
             } else {
-               // console.log("Failed to send Missing Timesheet Report email.");
+               // console.log("Failed to send Submit Timesheet Report email.");
                 parentPort.postMessage(`❌ Failed to send email to: ${row.staff_email}`);
             }
     } catch (err) {
