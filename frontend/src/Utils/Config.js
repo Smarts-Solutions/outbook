5c85b5b2dbd57b5d41d645b47c5111e63c0897fc
SHKGIT007 <shkgit007@gmail.com>
2025-10-06 17:06:20 +0530

    Done

--- frontend/src/Utils/Config.js
+++ frontend/src/Utils/Config.js
@@ -1,14 +1,14 @@
 // export const base_url = `${window.location.origin}:3001/`;
 // export const base_url = `${window.location.origin}/backend/`;
 // export const base_url = `http://localhost:2222/`;
-export const base_url = `https://dev.jobs.outbooks.com/backend/`;
+// export const base_url = `https://dev.jobs.outbooks.com/backend/`;
 // export const base_url = `https://jobs.outbooks.com/backend/`;
 
 
-// const isLocalhost = window.location.hostname === "localhost";
-// export const base_url = isLocalhost
-// ? "http://localhost:2222/"
-// : `${window.location.origin}/backend/`;
+const isLocalhost = window.location.hostname === "localhost";
+export const base_url = isLocalhost
+? "http://localhost:2222/"
+: `${window.location.origin}/backend/`;
 
 
 
