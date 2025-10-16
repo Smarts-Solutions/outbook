6aed6d6410581fbef8b9d454f81d9108766068fa
SHKGIT007 <shkgit007@gmail.com>
2025-10-16 15:36:50 +0530

    Done

--- backend/app/cron/missingTimesheetReportEmail.js
+++ backend/app/cron/missingTimesheetReportEmail.js
@@ -12,7 +12,7 @@ parentPort.on("message", async (rows) => {
     try {
             const [[getStaffNameMissingReport]] = await pool.execute(`CALL GetLastWeekMissingTimesheetReport(${row.id})`);
             //console.log("getStaffNameMissingReport , ",getStaffNameMissingReport); 
-            
+            if(getStaffNameMissingReport && getStaffNameMissingReport.length > 0){
             let csvContent = "Staff Name,Staff Email\n";
             getStaffNameMissingReport?.forEach(val => {
                 csvContent += `${val.staff_fullname},${val.staff_email}\n`;
@@ -35,6 +35,11 @@ parentPort.on("message", async (rows) => {
                // console.log("Failed to send Missing Timesheet Report email.");
                 parentPort.postMessage(`❌ Failed to send email to: ${row.staff_email}`);
             }
+          } else {
+            parentPort.postMessage(`ℹ️ No missing timesheet report for ${row.staff_email}`);
+          }
+
+
     } catch (err) {
       parentPort.postMessage(`❌ Failed for ${row.id}: ${err.message}`);
     }
--- backend/app/cron/submitTimesheetReminderEmail.js
+++ backend/app/cron/submitTimesheetReminderEmail.js
@@ -11,7 +11,7 @@ parentPort.on("message", async (rows) => {
     try {
             const [[getStaffNameSubmitReport]] = await pool.execute(`CALL GetLastWeekSubmitTimesheetReport(${row.id})`);
             //console.log("getStaffNameSubmitReport , ",getStaffNameSubmitReport); 
-            
+            if(getStaffNameSubmitReport && getStaffNameSubmitReport.length > 0){
             let csvContent = "Staff Name,Staff Email\n";
             getStaffNameSubmitReport?.forEach(val => {
                 csvContent += `${val.staff_fullname},${val.staff_email}\n`;
@@ -35,6 +35,13 @@ parentPort.on("message", async (rows) => {
                // console.log("Failed to send Submit Timesheet Report email.");
                 parentPort.postMessage(`❌ Failed to send email to: ${row.staff_email}`);
             }
+
+          } else {
+            parentPort.postMessage(`ℹ️ No submit timesheet report for ${row.staff_email}`);
+          }
+
+
+
     } catch (err) {
       parentPort.postMessage(`❌ Failed for ${row.id}: ${err.message}`);
     }
