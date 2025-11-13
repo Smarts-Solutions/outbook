e7d413a8921356494baebde780ff43589c0882dd
SHKGIT007 <shkgit007@gmail.com>
2025-11-13 14:35:38 +0530

    Done

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json());
 require("./app/routes")(app);
 // require("./app/cron/cron")(app);
 
+
 // Get Token Process
 // const client_id = "376ee1a2-3c24-48ac-b7cc-9a09f66b9e21";
 // const tenant_id = "332dcd89-cd37-40a0-bba2-a2b91abd434a";
