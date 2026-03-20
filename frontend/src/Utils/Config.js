663f969d400f40c591fc778d83ad89d6b2718649
SHKGIT007 <shkgit007@gmail.com>
2026-03-20 17:46:54 +0530

    Done

--- frontend/src/Utils/Config.js
+++ frontend/src/Utils/Config.js
@@ -4,6 +4,7 @@
 // export const base_url = `https://dev.jobs.outbooks.com/backend/`;
 // export const base_url = `https://jobs.outbooks.com/backend/`;
 
+
 const isLocalhost = window.location.hostname === "localhost";
 export const base_url = isLocalhost
 ? "http://localhost:2222/"
