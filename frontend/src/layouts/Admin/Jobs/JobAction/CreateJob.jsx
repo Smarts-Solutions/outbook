778e6840346c0ad9e2da8355b497e6a05a339354
SHKGIT007 <shkgit007@gmail.com>
2026-03-26 16:48:07 +0530

    Done

--- frontend/src/layouts/Admin/Jobs/JobAction/CreateJob.jsx
+++ frontend/src/layouts/Admin/Jobs/JobAction/CreateJob.jsx
@@ -3296,7 +3296,7 @@ const CreateJob = () => {
                                         value={
                                           jobData.processing_checkList
                                         }
-                                      > 
+                                      >
                                         <option value={null}>-- Select --</option>
                                         {AllJobData?.data?.processing_checklist_data?.map((item, index) => (
                                           <option key={index} value={item.id}>
@@ -3323,11 +3323,30 @@ const CreateJob = () => {
                                         }
                                       >
                                         <option value={null}>-- Select --</option>
-                                        {AllJobData?.data?.reviewing_checklist_data?.map((item, index) => (
-                                          <option key={index} value={item.id}>
-                                            {item.check_list_name}
-                                          </option>
-                                        ))}
+                                        {AllJobData?.data?.reviewing_checklist_data
+                                          ?.filter((item) => {
+
+                                            // Service Filter
+                                            let serviceMatch = true
+                                            if (item.service_id) {
+                                              const serviceIds = item.service_id.split(",").map(Number)
+                                              serviceMatch = serviceIds.includes(jobData.Service)
+                                            }
+
+                                            // Job Type Filter
+                                            let jobTypeMatch = true
+                                            if (item.job_type_id) {
+                                              const jobTypeIds = item.job_type_id.split(",").map(Number)
+                                              jobTypeMatch = jobTypeIds.includes(jobData.JobType)
+                                            }
+
+                                            return serviceMatch && jobTypeMatch
+                                          })
+                                          ?.map((item, index) => (
+                                            <option key={index} value={item.id}>
+                                              {item.check_list_name}
+                                            </option>
+                                          ))}
                                       </select>
 
                                     </div>
