9ade824b39861526de5f9be4b185c1dec32189e7
SHKGIT007 <shkgit007@gmail.com>
2026-04-13 16:58:44 +0530

    done

--- frontend/src/layouts/AuthWithAzure/AuthConfig.js
+++ frontend/src/layouts/AuthWithAzure/AuthConfig.js
@@ -15,7 +15,8 @@ const config = {
 
 
        // LIVE SETTINGS
-       clientId: '376ee1a2-3c24-48ac-b7cc-9a09f66b9e21', //  Application (client) ID - - CCCCC
+       clientId: '4b82f8d7-2832-45e6-85bd-a7cc12311df4', //  Application (client) ID - - CCCCC
+       //clientId: '376ee1a2-3c24-48ac-b7cc-9a09f66b9e21', //  Application (client) ID - - CCCCC
        authority: 'https://login.microsoftonline.com/common', //Accounts in any organizational directory and personal Microsoft account
        //redirectUri: 'https://outbooks.tradestreet.in/', //  Redirect URI
     //    redirectUri: 'https://dev.jobs.outbooks.com/', //  Redirect URI
