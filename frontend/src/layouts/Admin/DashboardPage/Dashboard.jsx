3ebd3c5076b9e9f59527603af41ed7e6183c1445
jatshakti744 <er.shaktijat@gmail.com>
2026-05-28 13:44:18 +0530

    done

--- frontend/src/layouts/Admin/DashboardPage/Dashboard.jsx
+++ frontend/src/layouts/Admin/DashboardPage/Dashboard.jsx
@@ -743,6 +743,8 @@ const Dashboard = () => {
                             name="fromDate"
                             onChange={(e) => selectFilterValue(e)}
                             max={selectedToDate || undefined}
+                            onKeyDown={(e) => e.preventDefault()}
+                            onClick={(e) => e.target.showPicker()}
                           />
                         </div>
 
@@ -757,6 +759,8 @@ const Dashboard = () => {
                             name="toDate"
                             onChange={(e) => selectFilterValue(e)}
                             min={selectedFromDate || undefined}
+                            onKeyDown={(e) => e.preventDefault()}
+                            onClick={(e) => e.target.showPicker()}
                           />
                         </div>
                       </div>
