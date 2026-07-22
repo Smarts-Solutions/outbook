0c078a83089a25b1ffeacf9568f8242276b02e5d
jatshakti744 <er.shaktijat@gmail.com>
2026-07-22 16:26:07 +0530

    done

--- frontend/src/layouts/Admin/Timesheet/Timesheet.jsx
+++ frontend/src/layouts/Admin/Timesheet/Timesheet.jsx
@@ -2019,8 +2019,8 @@ const Timesheet = () => {
   return (
 
     <div className="container-fluid" style={{ position: "relative" }}>
-      {(loading || exporting) && (
-        <div className="overlay">
+      {(loading || exporting || staffDataAll.loading || staffDataWeekDataAll.loading || isExistStaffDataWeekDataAll.loading || staffDataWeekDataAllSubmitTImeSheet.loading) && (
+        <div className="overlay" style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: 0, zIndex: 9999 }}>
           <div className="loader"></div>
         </div>
       )}
