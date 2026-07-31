8411844e7fbb06434674848d30e2b092075216d2
jatshakti744 <er.shaktijat@gmail.com>
2026-07-31 09:07:03 +0530

    done

--- backend/app/models/customerModel.js
+++ backend/app/models/customerModel.js
@@ -1702,6 +1702,95 @@ async function getAllCustomerByClientIdAndJobIdFilter(rows, client_id, job_id) {
     }
 }
 
+const getCustomer_dropdown_delete_old_working = async (customer) => {
+    // const { StaffUserId ,staff_id } = customer;
+    let StaffUserId = customer.staff_id;
+    // Line Manager
+    const LineManageStaffId = await LineManageStaffIdHelperFunction(StaffUserId)
+
+    // Get Role
+    const rows = await QueryRoleHelperFunction(StaffUserId)
+
+    const [RoleAccess] = await pool.execute('SELECT * FROM `role_permissions` WHERE role_id = ? AND permission_id = ?', [rows[0].role_id, 33]);
+
+    // Condition with Admin And SuperAdmin
+    if (rows.length > 0 && (rows[0].role_name == "SUPERADMIN" || RoleAccess.length > 0)) {
+        const query = `
+    SELECT  
+    customers.id AS id,
+    customers.status AS status,
+    customers.form_process AS form_process,
+    customers.trading_name AS trading_name,
+    CONCAT(
+    'cust_', 
+    SUBSTRING(customers.trading_name, 1, 3), '_',
+    SUBSTRING(customers.customer_code, 1, 15)
+    ) AS customer_code
+FROM 
+    customers 
+ORDER BY 
+id DESC;`;
+
+        const [result] = await pool.execute(query);
+
+        return { status: true, message: 'Success..', data: result };
+    }
+
+    // other Role Data
+    let query = `
+    SELECT  
+        customers.id AS id,
+        customers.customer_type AS customer_type,
+        customers.staff_id AS staff_id,
+        customers.account_manager_id AS account_manager_id,
+        customers.trading_name AS trading_name,
+        customers.trading_address AS trading_address,
+        customers.vat_registered AS vat_registered,
+        customers.vat_number AS vat_number,
+        customers.website AS website,
+        customers.form_process AS form_process,
+        customers.created_at AS created_at,
+        customers.updated_at AS updated_at,
+        customers.status AS status,
+        staff2.first_name AS account_manager_firstname, 
+        staff2.last_name AS account_manager_lastname,
+        customer_company_information.company_name AS company_name,
+        customer_company_information.company_number AS company_number,
+        CONCAT(
+            'cust_', 
+            SUBSTRING(customers.trading_name, 1, 3), '_',
+            SUBSTRING(customers.customer_code, 1, 15)
+        ) AS customer_code,
+        CASE
+        WHEN EXISTS (SELECT 1 FROM clients WHERE clients.customer_id = customers.id) 
+        THEN 1 ELSE 0
+        END AS is_client
+        FROM 
+            customers
+        JOIN 
+            staffs AS staff2 ON customers.account_manager_id = staff2.id
+        LEFT JOIN
+            assigned_jobs_staff_view ON assigned_jobs_staff_view.customer_id = customers.id
+        LEFT JOIN
+            customer_company_information ON customers.id = customer_company_information.customer_id
+        WHERE
+             customers.staff_id IN (${LineManageStaffId}) OR assigned_jobs_staff_view.staff_id IN (${LineManageStaffId})
+           GROUP BY customers.id
+           ORDER BY customers.id DESC
+
+         `;
+    try {
+        const [result] = await pool.execute(query);
+        return { status: true, message: 'Success..', data: result };
+    } catch (err) {
+        console.error('Error executing query getCustomer_dropdown:', err);
+        return { status: false, message: 'Error executing query', data: err };
+    }
+
+}
+
+
+
 const getCustomer_dropdown_delete = async (customer) => {
     // const { StaffUserId ,staff_id } = customer;
     let StaffUserId = customer.staff_id;
@@ -1788,6 +1877,8 @@ id DESC;`;
     }
 
 }
+
+
 
 // const getCustomer_dropdown_delete = async (customer) => {
 //     // const { StaffUserId ,staff_id } = customer;
