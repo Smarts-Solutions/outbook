79288ad6bcef0920fd5c94b1b7d8e241acf77e2a
jatshakti744 <er.shaktijat@gmail.com>
2026-08-10 10:08:23 +0530

    done

--- backend/app/models/staffsModel.js
+++ backend/app/models/staffsModel.js
@@ -1550,6 +1550,14 @@ const GetStaffPortfolio = async (staff) => {
           WHERE staff_id = ? 
              OR staff_id IN (${lineManageIn})
         )
+
+        UNION
+
+        SELECT customer_id FROM clients WHERE staff_created_id = ?
+
+        UNION
+
+        SELECT customer_id FROM jobs WHERE staff_created_id = ?
       `;
 
       const queryCustomerAssign = `
@@ -1560,7 +1568,7 @@ const GetStaffPortfolio = async (staff) => {
 
       const [assignedCustomers] = await connection.execute(
         queryCustomerAssign,
-        [id, id]
+        [id, id, id, id]
       );
 
       return assignedCustomers;
