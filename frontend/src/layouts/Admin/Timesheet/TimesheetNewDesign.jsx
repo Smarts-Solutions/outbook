3a578c84b238b19dff86c5c319b3223427f58464
jatshakti744 <er.shaktijat@gmail.com>
2026-09-07 17:45:25 +0530

    done

--- frontend/src/layouts/Admin/Timesheet/TimesheetNewDesign.jsx
+++ frontend/src/layouts/Admin/Timesheet/TimesheetNewDesign.jsx
@@ -2590,7 +2590,7 @@ const TimesheetNewDesign = () => {
                     <button className="timesheet-table-header-btn">
                       Clear hours
                     </button> */}
-                    <button type="button" className="timesheet-table-header-add-task-btn" onClick={handleAddNewSheet} disabled={isAddingRow || submitStatusAllKey === 1 || staffDetails.id != multipleFilter.staff_id}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M5 12h14"></path><path d="M12 5v14"></path></svg> Add task row</button>
+                    <button type="button" className="timesheet-table-header-add-task-btn" onClick={handleAddNewSheet} disabled={isAddingRow || staffDetails.id != multipleFilter.staff_id}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true"><path d="M5 12h14"></path><path d="M12 5v14"></path></svg> Add task row</button>
                     <button type="button" className="timesheet-table-header-btn" onClick={() => exportToCSV(timeSheetRows)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" x2="12" y1="15" y2="3"></line></svg> Export</button>
                   </div>
                 </div>
