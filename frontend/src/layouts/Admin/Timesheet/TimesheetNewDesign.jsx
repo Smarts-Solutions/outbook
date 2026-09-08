c9c990ade15bd3b7899d022216b2c59dc00d2e87
jatshakti744 <er.shaktijat@gmail.com>
2026-09-08 10:50:13 +0530

    done

--- frontend/src/layouts/Admin/Timesheet/TimesheetNewDesign.jsx
+++ frontend/src/layouts/Admin/Timesheet/TimesheetNewDesign.jsx
@@ -1099,36 +1099,26 @@ const TimesheetNewDesign = () => {
       let name = e.target.name;
 
 
-      let final_value = value;
-
-      let [intPart, decimalPart] = value.toString().split(".");
-
-      if (decimalPart) {
-        let multiplied = Math.floor(parseInt(decimalPart) * 0.6);
-
-        const multipliedStr = multiplied.toString().padStart(2, "0");
-        final_value = `${intPart}.${multipliedStr}`;
-        // final_value = `${intPart}.${multiplied}`;
-      }
-
-      // console.log(`final_value`, final_value);
-
       const updatedRows = [...timeSheetRows];
       if (updatedRows[index][name] == null) {
         updatedRows[index][name] = "";
         setTimeSheetRows(updatedRows);
       }
-      // if (!/^[0-9.]*$/.test(value)) {
-      //   return;
-      // }
+
       if (!/^\d*\.?\d{0,2}$/.test(value)) {
         return;
       }
 
-      if (parseFloat(final_value) > 23.59) {
+      let [integerPart, fractionalPartRaw] = value.toString().split(".");
+      let fractionalPart = fractionalPartRaw || "0";
+      if (fractionalPart.length === 1) {
+        fractionalPart = fractionalPart + "0";
+      }
+
+      if (parseInt(fractionalPart) > 59) {
         sweatalert.fire({
           icon: "warning",
-          title: "Total hours in a day cannot exceed 24",
+          title: "Minutes cannot exceed 59",
           timerProgressBar: true,
           showConfirmButton: true,
           timer: 1500,
@@ -1136,27 +1126,10 @@ const TimesheetNewDesign = () => {
         return;
       }
 
-      // const [integerPart, fractionalPart] = value.split(".");
-      // if (fractionalPart && parseInt(fractionalPart) > 59) {
-      //   sweatalert.fire({
-      //     icon: "warning",
-      //     title: "Minutes cannot exceed 59 ",
-      //     timerProgressBar: true,
-      //     showConfirmButton: true,
-      //     timer: 1500,
-      //   });
-      //   return;
-      // }
-
-      const [integerPart, fractionalPartRaw] = final_value.split(".");
-      let fractionalPart = fractionalPartRaw || "0";
-      if (fractionalPart.length === 1) {
-        fractionalPart = fractionalPart + "0";
-      }
-      if (parseInt(fractionalPart) > 59) {
+      if (parseFloat(value) > 23.59) {
         sweatalert.fire({
           icon: "warning",
-          title: "Minutes cannot exceed 59 ",
+          title: "Total hours in a day cannot exceed 24",
           timerProgressBar: true,
           showConfirmButton: true,
           timer: 1500,
